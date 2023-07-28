00fc96ace8e9622aed68ee823019552238aa61a9
AishaSK <aishasuhail2005@gmail.com>
2023-07-28 13:18:09 +0400

    Added imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
-
+import Header from './Header'
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import NewPost from './NewPost'
+import PostPage from './PostPage'
+import About from './About'
+import Missing from './Missing'
+import {Route, Switch, useHistory} from 'react-router-dom'
+import {useState, useEffect} from 'react'
 
 function App() {
   return (
