c756d4066db1c2bf8817a2cc499911459c51d6dc
AishaSK <aishasuhail2005@gmail.com>
2023-07-31 14:51:33 +0400

    feat: Implement PostPage component
    - Create PostPage component to display individual blog posts based on post ID.
    - If the post exists, display post title, date, and body.
    - Provide a "Delete Post" button with handleDelete functionality.
    - If the post is not found, show a "Post Not Found" message with a link to the homepage.

--- src/PostPage.js
+++ src/PostPage.js
@@ -1,11 +1,38 @@
 import React from 'react'
+/*The code imports both the useParams hook and the Link component 
+from react-router-dom. The useParams hook is used to access the URL 
+parameters, allowing us to get the id from the URL to find the specific post. */
 
-const PostPage = () => {
-  return (
-    <main>
-        <h1>PostPage</h1>
-    </main>
-  )
+import { useParams, Link } from "react-router-dom";
+
+const PostPage = ({ posts, handleDelete }) => {
+    const { id } = useParams();
+    const post = posts.find(post => (post.id).toString() === id);
+    return (
+        <main className="PostPage">
+            <article className="post">
+                {post &&
+                    <>
+                        <h2>{post.title}</h2>
+                        <p className="postDate">{post.datetime}</p>
+                        <p className="postBody">{post.body}</p>
+                        <button onClick={() => handleDelete(post.id)}>
+                            Delete Post
+                        </button>
+                    </>
+                }
+                {!post &&
+                    <>
+                        <h2>Post Not Found</h2>
+                        <p>Well, that's disappointing.</p>
+                        <p>
+                            <Link to='/'>Visit Our Homepage</Link>
+                        </p>
+                    </>
+                }
+            </article>
+        </main>
+    )
 }
 
-export default PostPage
+export default PostPage;
