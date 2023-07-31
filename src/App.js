3583aa6276ec4083479d84916114e4972fa9e861
AishaSK <aishasuhail2005@gmail.com>
2023-07-31 18:35:05 +0400

    Added CRUD w/ axios api

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
   const [postBody, setPostBody] = useState('');
+  const [editTitle, setEditTitle] = useState('');
+  const [editBody, setEditBody] = useState('');
   const navigate = useNavigate();
   
   useEffect(() => {
@@ -52,22 +54,50 @@ function App() {
     setSearchResults(filteredResults.reverse());
   }, [posts, search])
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const id = posts.length ? posts[posts.length - 1].id + 1 : 1;
     const datetime = format(new Date(), 'MMMM dd, yyyy pp');
     const newPost = { id, title: postTitle, datetime, body: postBody };
-    const allPosts = [...posts, newPost];
-    setPosts(allPosts);
-    setPostTitle('');
-    setPostBody('');
-    navigate('/');
+
+    try {
+      const response = await api.post('/posts', newPost)
+      const allPosts = [...posts, response.data];
+      setPosts(allPosts);
+      setPostTitle('');
+      setPostBody('');
+      navigate('/');
+    } catch (err){
+      console.log(`Error : ${err.message}`)
+    }
+
   }
 
-  const handleDelete = (id) => {
-    const postsList = posts.filter(post => post.id !== id);
-    setPosts(postsList);
-    navigate('/');
+  const handleEdit = async (id) => {
+
+    const datetime = format(new Date(), 'MMMM dd, yyyy pp');
+    const updatedPost = { id, title: editTitle, datetime, body: editBody };
+    try {
+        const response = await api.put(`/posts/${id}`, updatedPost)
+        setPosts(posts.map(post => post.id === id ? {...response.data} : post))
+        setEditTitle('')
+        setEditBody('')
+        navigate('/')
+    } catch (err){
+      console.log(`Error : ${err.message}`)
+    }
+    
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.delete(`/posts/${id}`)
+      const postsList = posts.filter(post => post.id !== id);
+      setPosts(postsList);
+      navigate('/');
+    } catch (err) {
+      console.log(`Error: ${err.message}`)
+    }
   }
 
   return (
--- src/api/posts.js
+++ src/api/posts.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'http://localhost:3500'
 })
 // to launch the json server 
 // npx json-server -p 3500 -w data/db.json
