e4252a832fc3877d551617b1e95e5e7fc8ce9de3
AishaSK <aishasuhail2005@gmail.com>
2023-08-01 15:01:41 +0400

    Updated about.js

--- src/About.js
+++ src/About.js
@@ -4,7 +4,19 @@ const About = () => {
   return (
       <main className='About'>
           <h2>About</h2>
-          <p style={{ marginTop: "1rem" }}>This blog app is made with the React JS frame work.</p>
+          <p style={{ marginTop: "1rem" }}>In this project, I've built a React-based web application that showcases the implementation of several concepts and tools.
+           I used the React library to create reusable and interactive components, 
+           allowing me to manage the user interface efficiently. 
+           
+           To fetch and manipulate data, I employed the Fetch API and Axios for making HTTP requests 
+           to a JSON Server, which simulates a backend API. 
+           
+           Additionally, React Router helped me handle navigation and routing within the application,
+            enabling multiple pages and URL parameter handling. 
+            Moreover, I utilized Flexbox for responsive and flexible component layout. 
+            
+            Throughout the project, I leveraged custom hooks to extract shared logic and promote 
+            code modularity and reusability.</p>
       </main>
   )
 }
